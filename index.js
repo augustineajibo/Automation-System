1490cfa7c1fc8fdbb8719a3adb6d1d4b81401025
AJIBO Augustine <103293432+augustineajibo@users.noreply.github.com>
2023-03-17 06:46:39 +0900

    Add files via upload
    This API is intended for reading the content of a CSV file and writing the same to elements in the User Interface(UI).

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const port = 5002;
+const port = 8002;
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/', function (req, res) {
--- recipe.js
+++ recipe.js
@@ -1,5 +1,5 @@
 function GetFileListing() {
-    var url = 'http://localhost:5002/recipes'
+    var url = 'http://localhost:8002/recipes'
 
     fetch(url)
     .then(function(response) {
@@ -8,7 +8,7 @@ function GetFileListing() {
     })
     .then(function(data) {
         console.log(data);
-        const dropdownElement = document.getElementById("Ref_recipe");
+        const dropdownElement = document.getElementById("Sample_type");
         data.forEach(function (option, i) {
             dropdownElement.add(new Option(option, option));
         });
@@ -16,6 +16,17 @@ function GetFileListing() {
     .catch(function(err) {
         console.log('Fetch Error :-S', err);
       });
+
+}
+function readCSV(){
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
+fs.createReadStream("./preset.csv")
+  .pipe(parse({ delimiter: ",", from_line: 2 }))
+  .on("data", function (row) {
+    console.log(row);
+  })
 
 }
 
